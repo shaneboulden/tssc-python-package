b9487e6b407753a28001b6f11ddc2b37825c893d
Hunter Gerlach <hgerlach@redhat.com>
2020-07-16 16:12:03 -0400

    WIP unit tests defined

--- tests/step_implementers/push_artifacts/test_maven_push_artifacts.py
+++ tests/step_implementers/push_artifacts/test_maven_push_artifacts.py
@@ -9,16 +9,352 @@ from tssc.step_implementers.push_artifacts import Maven
 
 from test_utils import *
 
-def test_tag_source_specify_maven_implementer():
+
+#def test_tag_source_specify_maven_implementer():
+#    with TempDirectory() as temp_dir:
+#        config = {
+#            'tssc-config': {    
+#                'push-artifacts': {
+#                    'implementer': 'Maven',
+#                    'config': {}
+#                }
+#            }
+#        }
+#        expected_step_results = {'tssc-results': {'push-artifacts': {}}}
+#
+#        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_artifact_id_missing():
     with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')
+        pom_file_path = os.path.join(temp_dir.path, 'pom.xml')
+# create temp config for this unit test w/ temp_pom
+        generate_metadata_config = {
+            'tssc-config': {
+                'generate-metadata': {
+                    'implementer': 'Maven'
+                 }
+            }
+        }
+        run_step_generate_metadata_for_tests(temp_dir, generate_metadata_config, runtime_args={'pom-file': str(pom_file_path)})
+
+# use temp_dir/results/generate-metadata.yaml
         config = {
             'tssc-config': {    
                 'push-artifacts': {
                     'implementer': 'Maven',
-                    'config': {}
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
                 }
             }
         }
-        expected_step_results = {'tssc-results': {'push-artifacts': {}}}
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        # run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results, ARGS[])
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+"""
+def test_push_artifact_with_file_missing():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
 
         run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_file_path_missing():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_group_id_missing():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_packaging_invalid():# TODO: Test for JAR/WAR/EAR
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_packaging_missing():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_repository_id_invalid():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_repository_id_missing():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_repository_url_invalid():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_repository_url_missing():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_version_malformed():# SNAPSHOT check
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+    <version>42.1-SNAPSHOT</version>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+def test_push_artifact_with_version_missing():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <artifactId>my-app</artifactId>
+    <groupId>com.mycompany.app</groupId>
+</project>''')  
+        config = {
+            'tssc-config': {    
+                'push-artifacts': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'packaging': 'jar',
+                        'repository': {
+                            'id': 'id',
+                            'url': 'url'
+                        }
+                    }
+                }
+            }
+        }
+        expected_step_results = {'tssc-results': {'push-artifacts': {'version': 'version'}}}
+
+        run_step_test_with_result_validation(temp_dir, 'push-artifacts', config, expected_step_results)
+
+
+#mvn deploy:deploy-file \
+#-DgroupId=org.acme \
+#-DartifactId=rest-json-quickstart \
+#-Dversion=1.0.2-ce5f284-SNAPSHOT \
+#-Dpackaging=jar \
+#-Dfile=target/tssc-reference-app-quarkus-rest-jsont-1.0-SNAPSHOT.jar \
+#-DrepositoryId=snapshots \
+#-Durl=http://artifactory.apps.tssc.rht-set.com/artifactory/libs-snapshot/
+"""
